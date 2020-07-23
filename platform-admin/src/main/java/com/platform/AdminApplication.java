3ddc12cca4b567b905e1921239a5232ff93d4a56
foquanlin <foquanlin@163.com>
2020-07-23 20:06:14 +0800

    修改

--- platform-admin/src/main/java/com/platform/AdminApplication.java
+++ platform-admin/src/main/java/com/platform/AdminApplication.java
@@ -3,7 +3,7 @@ package com.platform;
 //import com.platform.modules.job.config.ScheduleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
