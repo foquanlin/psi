b36587ea373a62e201c7227ae4cbf104af71ed64
foquanlin <foquanlin@163.com>
2020-08-12 22:21:50 +0800

    解决swagger 没有数据显示的问题

--- platform-common/src/main/java/com/platform/config/WebConfigurer.java
+++ platform-common/src/main/java/com/platform/config/WebConfigurer.java
@@ -24,30 +24,30 @@ public class WebConfigurer {
     /**
      * @return
      */
+    @Bean
+    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        //设置日期格式
+        ObjectMapper objectMapper = new ObjectMapper();
+        //反序列化忽略实体中不存在的参数
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //jackson时间格式化
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        //设置中文编码格式
+        List<MediaType> list = new ArrayList<MediaType>();
+        list.add(MediaType.APPLICATION_JSON);
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+        return mappingJackson2HttpMessageConverter;
+    }
+    //Gson解析有点问题,格式跟jackson对不上
 //    @Bean
-//    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
-//        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-//        //设置日期格式
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        //反序列化忽略实体中不存在的参数
-//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        //jackson时间格式化
-//        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-//        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
-//        //设置中文编码格式
+//    public GsonHttpMessageConverter gsonHttpMessageConverter(){
+//        GsonHttpMessageConverter convert = new GsonHttpMessageConverter();
+//        convert.setGson(new Gson());
 //        List<MediaType> list = new ArrayList<MediaType>();
 //        list.add(MediaType.APPLICATION_JSON_UTF8);
-//        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
-//        return mappingJackson2HttpMessageConverter;
+//        convert.setSupportedMediaTypes(list);
+//        return convert;
 //    }
-
-    @Bean
-    public GsonHttpMessageConverter gsonHttpMessageConverter(){
-        GsonHttpMessageConverter convert = new GsonHttpMessageConverter();
-        convert.setGson(new Gson());
-        List<MediaType> list = new ArrayList<MediaType>();
-        list.add(MediaType.APPLICATION_JSON_UTF8);
-        convert.setSupportedMediaTypes(list);
-        return convert;
-    }
 }
