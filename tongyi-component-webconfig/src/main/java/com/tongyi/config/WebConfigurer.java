e65768173f6a7640abe3b530dd7c2f11b48a4030
foquanlin <foquanlin@163.com>
2021-08-21 02:07:21 +0800

    修改配置

--- tongyi-component-webconfig/src/main/java/com/tongyi/config/WebConfigurer.java
+++ tongyi-component-webconfig/src/main/java/com/tongyi/config/WebConfigurer.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +30,7 @@ public class WebConfigurer {
         //反序列化忽略实体中不存在的参数
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         //jackson时间格式化
-//        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
         //设置中文编码格式
         List<MediaType> list = new ArrayList<MediaType>();
