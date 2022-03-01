db38a7f6233729e1e76814bf765e49c4c968c65c
foquanlin <foquanlin@163.com>
2022-03-02 00:54:16 +0800

    gx

--- tongyi-component-utils/src/main/java/com/tongyi/common/utils/JsonUtils.java
+++ tongyi-component-utils/src/main/java/com/tongyi/common/utils/JsonUtils.java
@@ -3,6 +3,9 @@ package com.tongyi.common.utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * @author 林佛权
  */
@@ -12,5 +15,12 @@ public class JsonUtils {
                 .setPrettyPrinting()
                 .create();
         return gson.toJson(obj);
+    }
+
+    public static <T> T fromJson(String json,Class<T> t){
+        return new GsonBuilder().create().fromJson(json,t);
+    }
+    public static <T> T fromJson(InputStream in, Class<T> t){
+        return new GsonBuilder().setPrettyPrinting().create().fromJson(new InputStreamReader(in),t);
     }
 }
