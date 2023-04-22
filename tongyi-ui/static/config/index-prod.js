994b540a6ae9dd8bd21880b568f262f9af845e49
foquanlin@163.com <foquanlin@163.com>
2023-04-23 02:05:50 +0800

    更新服务器地址

--- tongyi-ui/static/config/index-prod.js
+++ tongyi-ui/static/config/index-prod.js
@@ -4,7 +4,7 @@
 ;(function () {
   window.SITE_CONFIG = {}
 
-  window.SITE_CONFIG['baseUrl'] = 'http://admin.psi.wxngrok.com/admin'// 后台接口请求地址
+  window.SITE_CONFIG['baseUrl'] = 'http://psi.wxngrok.com/admin'// 后台接口请求地址
 
   // cdn地址 = 域名 + 版本号
   // window.SITE_CONFIG['domain'] = 'https://platform-wxmall.oss-cn-beijing.aliyuncs.com/boot/' // 域名
