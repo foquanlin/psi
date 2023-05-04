8afc879502c5ca35f666f21e5d0b2018d0a0eaf4
foquanlin@163.com <foquanlin@163.com>
2023-05-04 13:11:29 +0800

    增加客户对账单 增加供应商对账单

---
+++ tongyi-ui/src/views/modules/psi/report-bill-customer.vue
@@ -0,0 +1,3 @@
+<template>
+  <el-empty description="客户对账"><div style="font-size: 46px;">客户对账,付费版开放</div></el-empty>
+</template>
---
+++ tongyi-ui/src/views/modules/psi/report-bill-supplier.vue
@@ -0,0 +1,3 @@
+<template>
+  <el-empty description="供应商对账"><div style="font-size: 46px;">供应商对账,付费版开放</div></el-empty>
+</template>
