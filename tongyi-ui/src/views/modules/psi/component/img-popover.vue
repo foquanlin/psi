57a0c6af155d35426b9cfd6fe008bbc6d667e711
foquanlin@163.com <foquanlin@163.com>
2024-03-07 15:58:25 +0800

    增加图片显示组件

---
+++ tongyi-ui/src/views/modules/psi/component/img-popover.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="text-align: left;margin-left: 1px;width: 100%">
+    <el-popover placement="right-start" trigger="hover" style="width: 100%;" v-if="urls && urls !== ''">
+      <el-image fit="contain" style="width: 400px;" @click="openImg(urls)" :src="urls"/>
+      <img slot="reference" style="height: 50px;width: 50px;" :src="urls"/>
+    </el-popover>
+    {{name}}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true
+    },
+    urls: {
+      type: String,
+      require: false
+    }
+  }
+}
+</script>
